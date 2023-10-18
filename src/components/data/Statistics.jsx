a1d17accfbddd388bfb15157ea12c5d8049f9aaa
lirikape <lirikape@gmail.com>
2023-10-18 18:51:25 +0300

    hw

--- src/components/data/Statistics.jsx
+++ src/components/data/Statistics.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import styles from './Statistics.module.css';
 
 const Statistics = ({ title, stats }) => (
-  <section className={styles.statistics}> {/* Використовуємо стиль для секції */}
-    {title && <h2 className={styles.title}>{title}</h2>} {/* Використовуємо стиль для заголовку */}
-    <ul className={styles.statList}> {/* Використовуємо стиль для списку */}
+  <section className={styles.statistics}> 
+    {title && <h2 className={styles.title}>{title}</h2>} 
+    <ul className={styles.statList}> 
       {stats.map(({ id, label, percentage }) => (
-        <li key={id} className={styles.item}> {/* Використовуємо стиль для елемента списку */}
-          <span className={styles.label}>{label}</span> {/* Використовуємо стиль для мітки */}
-          <span className={styles.percentage}>{percentage}%</span> {/* Використовуємо стиль для відсотка */}
+        <li key={id} className={styles.item}> 
+          <span className={styles.label}>{label}</span> 
+          <span className={styles.percentage}>{percentage}%</span> 
         </li>
       ))}
     </ul>
